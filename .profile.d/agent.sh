3787317db1b564d725cb129a397c8060b66f6e23
Bernhard Graf <bernhard.graf@klickrent.com>
2016-05-06 13:57:37 +0200

    "readlink -m" is not portable.

--- .profile.d/agent.sh
+++ .profile.d/agent.sh
@@ -7,7 +7,7 @@
 # set up SSH agent socket symlink
 export SSH_AUTH_SOCK_LINK="/tmp/ssh-$USER/agent"
 
-if ! [ -r $(readlink -m $SSH_AUTH_SOCK_LINK) ] && [ -r $SSH_AUTH_SOCK ]; then
+if ! [ -r $(readlink $SSH_AUTH_SOCK_LINK) ] && [ -r $SSH_AUTH_SOCK ]; then
   mkdir -p "$(dirname $SSH_AUTH_SOCK_LINK)" &&
   chmod go= "$(dirname $SSH_AUTH_SOCK_LINK)" &&
   ln -sfn $SSH_AUTH_SOCK $SSH_AUTH_SOCK_LINK
