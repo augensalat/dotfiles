51869d49f86c35c68996d8703504a9afa2ec31cd
Bernhard Graf <bernhard.graf@klickrent.com>
2016-05-09 14:59:31 +0200

    Do not insist on vim living in /usr/bin.

--- .bash.d/env.sh
+++ .bash.d/env.sh
@@ -2,5 +2,5 @@
 
 # Some applications read the EDITOR variable to determine your favourite text
 # editor. So edit the line below and enter the editor of your choice. :-)
-export EDITOR=/usr/bin/vim
+export EDITOR=vim
 
