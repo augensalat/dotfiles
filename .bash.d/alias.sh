689059725a01b8ea731ca2188331baee09878104
Bernhard Graf <bernhard.graf@klickrent.com>
2016-05-09 17:38:46 +0200

    Set common aliases.

--- .bash.d/alias.sh
+++ .bash.d/alias.sh
@@ -4,4 +4,17 @@
 # Set aliases
 #
 
+alias o='less'
+alias ..='cd ..'
+alias ...='cd ../..'
+alias cd..='cd ..'
+alias -- +='pushd .'
+alias -- -='popd'
+alias egrep='egrep --color=auto'
+alias fgrep='fgrep --color=auto'
+alias grep='grep --color=auto'
+alias md='mkdir -p'
+alias o='less'
+alias rd='rmdir'
+alias rehash='hash -r'
 alias find-ssh-agent='eval $(~/bin/find-ssh-agent.sh)'
