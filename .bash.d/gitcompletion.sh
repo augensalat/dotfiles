e14105e2dd88f6aefc4ffb4dde45e3003ce61cff
Bernhard Graf <bernhard.graf@klickrent.com>
2016-09-15 13:06:00 +0200

    Use my own git bash completion
    The one that comes with XCode issues annoing warnings after 'git prune'
    or 'git fetch -p'.

--- .bash.d/gitcompletion.sh
+++ .bash.d/gitcompletion.sh
@@ -2,7 +2,9 @@
 
 case "$OSTYPE" in
   darwin*)
-    if [ -f /Applications/Xcode.app/Contents/Developer/usr/share/git-core/git-completion.bash ]; then
+    if [ -f $HOME/bin/git-completion.bash ]; then
+      . $HOME/bin/git-completion.bash
+    elif [ -f /Applications/Xcode.app/Contents/Developer/usr/share/git-core/git-completion.bash ]; then
       . /Applications/Xcode.app/Contents/Developer/usr/share/git-core/git-completion.bash
     elif [ -f $(brew --prefix)/etc/bash_completion ]; then
       . $(brew --prefix)/etc/bash_completion
